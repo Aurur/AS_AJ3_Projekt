a889d92e4a48219621088c73ac2f7dc44e1fe357
Aurur <andreas.lau@gmx.eu>
2014-09-17 14:20:56 +0200

    Tennisverein.java hinzugefügt
    zum speichern in XML-Datei notwendig

---
+++ TennsvereinVerwaltung/src/repository/Tennisverein.java
@@ -0,0 +1,13 @@
+package repository;
+
+import java.util.List;
+
+/**
+ * Kurzbeschreibung der Klasse
+ * @version 1.0 vom 17.09.2014
+ * @author Manfred Mustermann
+ */
+public class Tennisverein {
+  List<Member> memberList;
+  List<Game> gameList;
+}

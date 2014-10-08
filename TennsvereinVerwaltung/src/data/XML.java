57f697113ffdef2a944cb0131df32aae07070fbc
Aurur <andreas.lau@gmx.eu>
2014-10-08 13:46:15 +0200

    XML
    xml-datei angepasst + sqllite.jar hinzugefügt

--- TennsvereinVerwaltung/src/data/XML.java
+++ TennsvereinVerwaltung/src/data/XML.java
@@ -10,7 +10,6 @@ import java.beans.XMLDecoder;
 import java.beans.XMLEncoder;
 import repository.Game;
 import repository.Member;
-import repository.Tennisverein;
 
 /**
  * Kurzbeschreibung der Klasse
@@ -18,72 +17,93 @@ import repository.Tennisverein;
  * @author Manfred Mustermann
  */
 public class XML implements IData {
-  
-  private Tennisverein tv = new Tennisverein();
-  
-  public void serializeObjectToXML(String xmlFileLocation, Object objectToSerialize) throws Exception {
-          FileOutputStream os = new FileOutputStream(xmlFileLocation);
-          XMLEncoder encoder = new XMLEncoder(os);
-          encoder.writeObject(objectToSerialize);
-          encoder.close();
-      }
-   
-      /**
-       * Reads Java Bean Object From XML File
-       */
-  public Object deserializeXMLToObject(String xmlFileLocation) throws Exception {
-          FileInputStream os = new FileInputStream(xmlFileLocation);
-          XMLDecoder decoder = new XMLDecoder(os);
-          Object deSerializedObject = decoder.readObject();
-          decoder.close();
-   
-          return deSerializedObject;
+	List<Member> listOfMembers;
+	XML(){
+		listOfMembers = getListOfMembers();
+	}
+  /**
+   * This method saves (serializes) any java bean object into xml file
+   */
+  private void serializeObjectToXML(String xmlFileLocation,
+      Object objectToSerialize) throws Exception {
+      FileOutputStream os = new FileOutputStream(xmlFileLocation);
+      XMLEncoder encoder = new XMLEncoder(os);
+      encoder.writeObject(objectToSerialize);
+      encoder.close();
   }
-      
+
+  /**
+   * Reads Java Bean Object From XML File
+   */
+  private Object deserializeXMLToObject(String xmlFileLocation)
+          throws Exception {
+      FileInputStream os = new FileInputStream(xmlFileLocation);
+      XMLDecoder decoder = new XMLDecoder(os);
+      Object deSerializedObject = decoder.readObject();
+      decoder.close();
+
+      return deSerializedObject;
+  }
+  
   @Override
   public boolean createMember(String lastname, String firstname) {
+    Member member = new Member(lastname, firstname);
+    	listOfMembers.add(member);
     try {
-      
-      List<Member> memberList = tv.getMemberList();
-      memberList.add(new Member(lastname,firstname));
-      tv.setMemberList(memberList);
-      
-      serializeObjectToXML("Tennisverein.xml", tv);
-      
-    } catch (Exception e2) {
+      serializeObjectToXML("Member.xml", listOfMembers);
+    } catch (Exception e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
       return false;
     }
-    
     return true;
   }
 
   @Override
   public List<Member> getListOfMembers() {
-    // TODO Auto-generated method stub
-    return null;
+		List<Member> member = null;
+		  try {
+			member = (List<Member>) deserializeXMLToObject("Member.xml");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    return member;
   }
 
   @Override
   public boolean createGame(Date date, Member memberOne, Member memberTwo) {
+    XMLEncoder e;
     try {
-      
-      List<Game> gameList = tv.getGameList();
-      gameList.add(new Game(date, memberOne, memberTwo));
-      tv.setGameList(gameList);
-      
-      serializeObjectToXML("Tennisverein.xml", tv);
-      
-    } catch (Exception e2) {
+      e = new XMLEncoder(new BufferedOutputStream(new FileOutputStream("Test.xml")));
+    } catch (FileNotFoundException e1) {
       return false;
     }
-    
+    e.writeObject("test1");
+    e.close();
     return true;
   }
 
   @Override
   public List<Game> getListOfGames(Member member) {
-    // TODO Auto-generated method stub
+    List<Game> allGames = getAllGames();
+    for(Game game:allGames){
+    	if(!game.getMemberOne().equals(member) && !game.getMemberTwo().equals(member)){
+    		allGames.remove(game);
+    	}
+    }
     return null;
   }
-
+ 
+  public List<Game> getAllGames() {
+	List<Game> games = null;
+	  try {
+		games = (List<Game>) deserializeXMLToObject("Games.xml");
+	} catch (Exception e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	return games;
+  }
+  
 }

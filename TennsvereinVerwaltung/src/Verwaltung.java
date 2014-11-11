eb3eaf1b6d9302df2b4d2072b560ac79adeaf402
Nyria <vlfuhl@hotmail.de>
2014-11-11 12:10:04 +0100

    Create Verwaltung.java

---
+++ TennsvereinVerwaltung/src/Verwaltung.java
@@ -0,0 +1,19 @@
+import controller.Controller;
+import UI.TUI;
+
+/**
+ * Kurzbeschreibung der Klasse
+ * @version 1.0 vom 08.10.2014
+ * @author Manfred Mustermann
+ */
+public class Verwaltung {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		TUI eineTUI = new TUI(new Controller());
+
+	}
+
+}

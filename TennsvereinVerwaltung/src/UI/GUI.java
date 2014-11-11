951cd1b709e82ec443a4b6efd746ab9873a49e32
Nyria <vlfuhl@hotmail.de>
2014-11-11 12:06:12 +0100

    Create GUI.java

---
+++ TennsvereinVerwaltung/src/UI/GUI.java
@@ -0,0 +1,17 @@
+package UI;
+
+import controller.Controller;
+/**
+* FA21
+ * @version 1.0 vom 17.09.2014
+ * @author Andreas Lau, Vivian Fuhl
+ */
+public class GUI {
+
+	private Controller controller;
+	
+	public GUI(Controller controller){
+		this.controller = controller;
+		
+	}
+}
